1ba9017b19e3b45cadb5575fc13c864eab58032a
denis <dinka14@yandex.ru>
2019-04-23 16:28:35 +0300

    next tasks

---
+++ elementary/digits-multiplication.py
@@ -0,0 +1,18 @@
+def checkio(number: int) -> int:
+    mult = 1
+    for i in str(number):
+        if i != '0':
+            mult *= int(i)
+    return mult
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio(123405))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
