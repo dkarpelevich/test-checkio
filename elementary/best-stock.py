8c04e34da69953c817bc46fa5d485743977c76c3
denis <dinka14@yandex.ru>
2019-04-23 17:12:40 +0300

    next tasks

---
+++ elementary/best-stock.py
@@ -0,0 +1,26 @@
+def best_stock(data):
+    max_value = max(data.values())
+    for i in data:
+        if data[i] == max_value:
+            return i
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(best_stock({
+        'CAC': 10.0,
+        'ATX': 390.2,
+        'WIG': 1.2
+    }))
+
+    assert best_stock({
+        'CAC': 10.0,
+        'ATX': 390.2,
+        'WIG': 1.2
+    }) == 'ATX', "First"
+    assert best_stock({
+        'CAC': 91.1,
+        'ATX': 1.01,
+        'TASI': 120.9
+    }) == 'TASI', "Second"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
