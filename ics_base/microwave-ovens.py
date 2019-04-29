2dc8d264ed42c22d4da6f6da29d65d2c1f5bf401
denis <dinka14@yandex.ru>
2019-04-29 18:51:19 +0300

    next tasks

--- ics_base/microwave-ovens.py
+++ ics_base/microwave-ovens.py
@@ -1,6 +1,5 @@
 from abc import abstractmethod
 
-
 class MicrowaveBase:
     def __init__(self):
         self.time = '00:00'
@@ -12,13 +11,15 @@ class MicrowaveBase:
 
 class Microwave1(MicrowaveBase):
     def show(self):
-        self.time = '_' + self.time[1:]
+        self.time = '{0}{1}'.format('_', self.time[1:])
         return self.time
+
 
 class Microwave2(MicrowaveBase):
     def show(self):
-        self.time = self.time[:len(self.time)-1] + '_'
+        self.time = '{0}{1}'.format(self.time[:len(self.time) - 1], '_')
         return self.time
+
 
 class Microwave3(MicrowaveBase):
     def show(self):
@@ -26,48 +27,43 @@ class Microwave3(MicrowaveBase):
 
 
 class RemoteControl:
-    def __init__(self, microwave):
+    def __init__(self, microwave: MicrowaveBase):
         self.__microwave = microwave
-        self.microwave_seconds = 0
-        self.time_in_seconds = 0
+        self.__time_on_microwave_seconds = 0
+        self.__time_in_seconds = 0
 
     def show_time(self):
+        m, s = divmod(self.__time_on_microwave_seconds, 60)
+        self.__microwave.time = str('{:02d}:{:02d}'.format(m, s))
         return self.__microwave.show()
 
-    def set_time(self, time):
+    def set_time(self, time: str):
         self.__microwave.time = time
-        minute, sec = self.__microwave.time.split(':')
-        self.microwave_seconds = int(minute) * 60 + int(sec)
+        minute, sec = [int(i) for i in self.__microwave.time.split(':')]
+        self.__time_on_microwave_seconds = minute * 60 + sec
 
-    def add_time(self, time):
-        self.time_in_seconds = (int(time[:time.rindex('s')]) if time.endswith('s')
-                                else int(time[:time.rindex('m')])*60)
+    @staticmethod
+    def convert_time_to_seconds(time: str):
+        return (int(time[:time.rindex('s')]) if time.endswith('s')
+                else int(time[:time.rindex('m')]) * 60)
 
-        add_minutes = (self.microwave_seconds + self.time_in_seconds) // 60
-        add_seconds = (self.microwave_seconds + self.time_in_seconds) - 60 * add_minutes
-        if self.microwave_seconds + self.time_in_seconds >= 5400:
-            self.__microwave.time = '90:00'
-        else:
-            self.__microwave.time = '{0}:{1}'.format(str("{:02d}".format(add_minutes)),
-                                                     str("{:02d}".format(add_seconds)))
+    def add_time(self, time: str):
+        self.__time_in_seconds = RemoteControl.convert_time_to_seconds(time)
+        self.__time_on_microwave_seconds = self.__time_on_microwave_seconds + self.__time_in_seconds
+        if self.__time_on_microwave_seconds >= 5400:
+            self.__time_on_microwave_seconds = 5400
 
-    def del_time(self, time):
-        self.time_in_seconds = (int(time[:time.rindex('s')]) if time.endswith('s')
-                                else int(time[:time.rindex('m')]) * 60)
-
-        decrease_minutes = (self.microwave_seconds - self.time_in_seconds) // 60
-        decrease_seconds = (self.microwave_seconds - self.time_in_seconds) - 60 * decrease_minutes
-        if self.microwave_seconds - self.time_in_seconds <= 0:
-            self.__microwave.time = '00:00'
-        else:
-            self.__microwave.time = '{0}:{1}'.format(str("{:02d}".format(decrease_minutes)),
-                                                     str("{:02d}".format(decrease_seconds)))
+    def del_time(self, time: str):
+        self.__time_in_seconds = RemoteControl.convert_time_to_seconds(time)
+        self.__time_on_microwave_seconds = self.__time_on_microwave_seconds - self.__time_in_seconds
+        if self.__time_on_microwave_seconds <= 0:
+            self.__time_on_microwave_seconds = 0
 
 
 if __name__ == '__main__':
-    microwave_1 = Microwave1()
-    rc_1 = RemoteControl(microwave_1)
-    rc_1.set_time("05:33")
-    rc_1.del_time("30s")
-    rc_1.del_time("2m")
-    rc_1.show_time()
+    microwave_2 = Microwave2()
+    rc_2 = RemoteControl(microwave_2)
+    rc_2.set_time("89:00")
+    rc_2.add_time("90s")
+    rc_2.add_time("20m")
+    rc_2.show_time()
