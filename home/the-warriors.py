700d992d6b3b9de33790f6b5a19b0d3d66fbc37e
denis <dinka14@yandex.ru>
2019-05-24 22:02:05 +0300

    next tasks

--- home/the-warriors.py
+++ home/the-warriors.py
@@ -2,16 +2,45 @@ class Warrior:
     def __init__(self):
         self.health = 50
         self.attack = 5
-        if self.health > 0:
-            self.is_alive = True
-        else:
-            self.is_alive = False
+        self.is_alive = True if self.health > 0 else False
+
 
 class Knight(Warrior):
     def __init__(self):
         super().__init__()
         self.health = 50
         self.attack = 7
+
+
+class Defender(Warrior):
+    def __init__(self):
+        super().__init__()
+        self.health = 60
+        self.attack = 3
+        self.defense = 2
+
+class Army:
+    def __init__(self):
+        self.list_of_units = []
+
+    def add_units(self, units, units_amount: int):
+        self.list_of_units += [units() for _ in range(units_amount)]
+
+
+class Battle:
+    @staticmethod
+    def fight(army_1, army_2) -> bool:
+        army_1_unit = len(army_1.list_of_units) - 1
+        army_2_unit = len(army_2.list_of_units) - 1
+        while army_2_unit >= 0 and army_1_unit >= 0:
+            if fight(army_1.list_of_units[army_1_unit], army_2.list_of_units[army_2_unit]):
+                army_2_unit -= 1
+            else:
+                army_1_unit -= 1
+            if army_2_unit < 0:
+                return True
+            elif army_1_unit < 0:
+                return False
 
 def fight(unit_1, unit_2) -> bool:
     while unit_1.health > 0 and unit_2.health > 0:
@@ -26,18 +55,21 @@ def fight(unit_1, unit_2) -> bool:
 
 
 if __name__ == '__main__':
-    chuck = Warrior()
-    bruce = Warrior()
-    carl = Knight()
-    dave = Warrior()
-    mark = Warrior()
+    # battle tests
+    my_army = Army()
+    my_army.add_units(Knight, 3)
 
-    assert fight(chuck, bruce) == True
-    assert fight(dave, carl) == False
-    assert chuck.is_alive == True
-    assert bruce.is_alive == False
-    assert carl.is_alive == True
-    assert dave.is_alive == False
-    assert fight(carl, mark) == False
-    assert carl.is_alive == False
+    enemy_army = Army()
+    enemy_army.add_units(Warrior, 3)
 
+    army_3 = Army()
+    army_3.add_units(Warrior, 20)
+    army_3.add_units(Knight, 5)
+
+    army_4 = Army()
+    army_4.add_units(Warrior, 30)
+
+    battle = Battle()
+
+    assert battle.fight(my_army, enemy_army) == True
+    assert battle.fight(army_3, army_4) == False
