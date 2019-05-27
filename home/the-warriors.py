96218856981c10bec2800338e4b8e005b380872e
denis <dinka14@yandex.ru>
2019-05-27 17:04:53 +0300

    next tasks

--- home/the-warriors.py
+++ home/the-warriors.py
@@ -19,6 +19,12 @@ class Defender(Warrior):
         self.attack = 3
         self.defense = 2
 
+class Rookie(Warrior):
+    def __init__(self):
+        super().__init__()
+        self.health = 50
+        self.attack = 1
+
 class Army:
     def __init__(self):
         self.list_of_units = []
@@ -44,32 +50,62 @@ class Battle:
 
 def fight(unit_1, unit_2) -> bool:
     while unit_1.health > 0 and unit_2.health > 0:
-        unit_2.health -= unit_1.attack
+        defense = unit_2.defense if isinstance(unit_2, Defender) else 0
+        if unit_1.attack > defense:
+            unit_2.health -= (unit_1.attack - defense)
         if unit_2.health <= 0:
             unit_2.is_alive = False
             return True
-        unit_1.health -= unit_2.attack
+        defense = unit_1.defense if isinstance(unit_1, Defender) else 0
+        if unit_2.attack > defense:
+            unit_1.health -= (unit_2.attack - defense)
         if unit_1.health <= 0:
             unit_1.is_alive = False
             return False
 
 
 if __name__ == '__main__':
-    # battle tests
+    unit_one = Warrior()
+    unit_two = Rookie()
+    # noinspection PyTypeChecker
+    fight(unit_one, unit_two)
+    print(unit_one.health)
+
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+    bob = Defender()
+    mike = Knight()
+    rog = Warrior()
+    lancelot = Defender()
+    # noinspection PyTypeChecker
+    assert fight(chuck, bruce) == True
+    assert fight(dave, carl) == False
+    assert chuck.is_alive == True
+    assert bruce.is_alive == False
+    assert carl.is_alive == True
+    assert dave.is_alive == False
+    assert fight(carl, mark) == False
+    assert carl.is_alive == False
+    assert fight(bob, mike) == False
+    assert fight(lancelot, rog) == True
+
     my_army = Army()
-    my_army.add_units(Knight, 3)
+    my_army.add_units(Defender, 1)
 
     enemy_army = Army()
-    enemy_army.add_units(Warrior, 3)
+    enemy_army.add_units(Warrior, 2)
 
     army_3 = Army()
-    army_3.add_units(Warrior, 20)
-    army_3.add_units(Knight, 5)
+    army_3.add_units(Warrior, 1)
+    army_3.add_units(Defender, 1)
 
     army_4 = Army()
-    army_4.add_units(Warrior, 30)
+    army_4.add_units(Warrior, 2)
 
     battle = Battle()
 
-    assert battle.fight(my_army, enemy_army) == True
-    assert battle.fight(army_3, army_4) == False
+    assert battle.fight(my_army, enemy_army) == False
+    assert battle.fight(army_3, army_4) == True
