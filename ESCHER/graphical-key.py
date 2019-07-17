859e0e0fdafc4541c8814e0b87436520d2c6d184
denis <dinka14@yandex.ru>
2019-07-17 17:01:30 +0300

    next tasks

--- ESCHER/graphical-key.py
+++ ESCHER/graphical-key.py
@@ -1,42 +1,54 @@
-from graph_module import graph_methods as gm
-
 def g_key(grid, path):
-    graph = gm.Graph.prepare_graph(grid, True, True)
-    path_list = list(gm.Graph.dfs_paths_recursion(graph, (0, 0), (len(grid)-1, len(grid[0])-1)))
-    short_path_list = []
-    for i in path_list:
-        if len(i) <= path:
-            short_path_list.append(i)
-    sum_island = [0 for _ in range(len(short_path_list))]
-    for item in range(len(short_path_list)):
-        for j in short_path_list[item]:
-            sum_island[item] += grid[j[0]][j[1]]
-    return max(sum_island)
+    H = len(grid)
+    W = len(grid[0])
+    valid = [(x, y) for x in range(H) for y in range(W)]
+    res = []
+
+    def pathfinder(x, y, step, vlist, Sm):
+        if not (x, y) in vlist: return 0
+        if path - step < H - x - 1 or path - step < W - y - 1: return 0
+        if step == path:
+            if x < H - 1 or y < W - 1: return 0
+            res.append(Sm + grid[x][y])
+            return 1
+        vlist.remove((x, y))
+        for (a, b) in [(0, -1), (0, 1), (-1, -1), (-1, 1), (-1, 0), (1, -1), (1, 0), (1, 1)]:
+            z = pathfinder(x + a, y + b, step + 1, vlist[:], Sm + grid[x][y])
+        return 1
+
+    if H * W == path: return sum([sum(x) for x in grid])
+
+    pathfinder(0, 0, 1, valid, 0)
+
+    return max(res)
+
 
 if __name__ == '__main__':
     print("Example:")
-    print(g_key([[1, 6, 7],
-                 [0, 4, 9],
-                 [2, 6, 1]], 4))
+    print(g_key([[1, 6, 7, 2, 4],
+                 [0, 4, 9, 5, 3],
+                 [7, 2, 5, 1, 4],
+                 [3, 3, 2, 2, 9],
+                 [2, 6, 1, 4, 0]], 9))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    # assert g_key([[1, 6, 7, 2, 4],
-    #               [0, 4, 9, 5, 3],
-    #               [7, 2, 5, 1, 4],
-    #               [3, 3, 2, 2, 9],
-    #               [2, 6, 1, 4, 0]], 9) == 46
-    #
-    # assert g_key([[2, 5, 4, 1, 8],
-    #               [0, 4, 9, 5, 3],
-    #               [7, 2, 5, 1, 4],
-    #               [3, 3, 2, 2, 9],
-    #               [2, 6, 1, 4, 1]], 6) == 27
-    #
-    # assert g_key([[1, 2, 3, 4, 5],
-    #               [2, 3, 4, 5, 1],
-    #               [3, 4, 5, 1, 2],
-    #               [4, 5, 1, 2, 3],
-    #               [5, 1, 2, 3, 4]], 25) == 75
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert g_key([[1, 6, 7, 2, 4],
+                  [0, 4, 9, 5, 3],
+                  [7, 2, 5, 1, 4],
+                  [3, 3, 2, 2, 9],
+                  [2, 6, 1, 4, 0]], 9) == 46
+
+    assert g_key([[2, 5, 4, 1, 8],
+                  [0, 4, 9, 5, 3],
+                  [7, 2, 5, 1, 4],
+                  [3, 3, 2, 2, 9],
+                  [2, 6, 1, 4, 1]], 6) == 27
+
+    assert g_key([[1, 2, 3, 4, 5],
+                  [2, 3, 4, 5, 1],
+                  [3, 4, 5, 1, 2],
+                  [4, 5, 1, 2, 3],
+                  [5, 1, 2, 3, 4]], 25) == 75
 
     assert g_key([[1, 6],
                   [5, 1]], 2) == 2
