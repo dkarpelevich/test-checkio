38f83ff3e9d022f6f6e892e0faa94043331ecd08
Denis Karpelevich <dkarpele@redhat.com>
2020-04-06 16:36:59 +0200

    next tasks

---
+++ Electronic_station/acceptable-password-ii.py
@@ -0,0 +1,19 @@
+# Taken from mission Acceptable Password I
+
+def is_acceptable_password(password: str) -> bool:
+    return True if len(password) > 6 and any(map(str.isdigit, password)) else False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == False
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
